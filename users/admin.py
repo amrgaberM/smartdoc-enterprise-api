0f463bcff006eb99bcdda5f3c62024adfd88a81f
amrgaberM <amrgabeerr20@gmail.com>
2026-02-05 23:56:48 +0200

    fix: Register CustomUser model in Django Admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+# This tells Django: "Use the standard User Admin interface for my custom User model"
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    # We will customize this later to show 'organization' etc.
+    pass
