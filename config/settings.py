735d4f6f0c9bf1d5239536020d160cacac7e9069
amrgaberM <amrgabeerr20@gmail.com>
2026-02-07 18:00:34 +0200

    Fix typo issue

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated', # Lock everything by default
     ],
- '  DEFAULT_AUTHENTICATION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',  # <--- NEW: The Ticket Reader
         'rest_framework.authentication.SessionAuthentication',      # Keep this for the Browser (Browsable API)
     ],
