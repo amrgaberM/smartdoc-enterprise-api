ca4f38625338bb5fb391ee07b6682a70d61151a5
amrgaberM <amrgabeerr20@gmail.com>
2026-02-07 17:57:04 +0200

    chore(security): move secrets to .env and rotate key

--- config/settings.py
+++ config/settings.py
@@ -12,16 +12,18 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y0jkx7*)ka=_x60^fhs4d81!233^r4ihv_#^93743mnfeh#)(v'
+SECRET_KEY = config('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', default=False, cast=bool)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
